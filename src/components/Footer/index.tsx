9b2fdf060dbcdd86274b5084083cb7f3508d0aee
Raluca Nicola <ralucagnicola@gmail.com>
2024-07-23 13:13:50 +0200

    Add github link

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -8,7 +8,8 @@ export const Footer = () => {
         <a href='https://sketchfab.com/3d-models/yellow-submarine-0dcb53b8f0734509a83a51e672d27dc4'>Submarine</a>,{' '}
         <a href='https://sketchfab.com/3d-models/yellow-submarine-0dcb53b8f0734509a83a51e672d27dc4'>wind turbine</a>,{' '}
         and <a href='https://sketchfab.com/3d-models/sailboat-76d0b1e24be14d2f9a524bfce3001aeb'>sailboat</a> provided by
-        Sketchfab under <a href='https://creativecommons.org/licenses/by/4.0/'>CC BY 4.0</a>
+        Sketchfab under <a href='https://creativecommons.org/licenses/by/4.0/'>CC BY 4.0</a>. Check out the code on{' '}
+        <a href='https://github.com/RalucaNicola/3D-diorama'>GitHub</a>.
       </p>
     </div>
   );
